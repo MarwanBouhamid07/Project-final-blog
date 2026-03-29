d91164f95b7cfa464817f2e2dfed1d7d5b7dc6af
MarwanBouhamid <marwanbouhamid123@gmail.com>
2026-03-29 14:26:03 +0100

    complete header

---
+++ assets/scripts/script.js
@@ -0,0 +1,5 @@
+let goToHomePage = document.getElementById("logo-project-name");
+
+goToHomePage.onclick = function(){
+    window.open("home.php","_self");
+}
---
+++ includes/header.php
@@ -0,0 +1,21 @@
+
+    <header>
+        <div class="group-style">
+        <div class="group-LNM-L">
+            <div class="logo-project-name" id="logo-project-name">
+                <div class="logo">PI</div>
+                <div class="project-name">Project Insight</div>
+            </div>
+            <nav>
+                <a href="">Home</a>
+                <a href="">Article</a>
+                <a href="">Showcase</a>
+            </nav>
+        </div>
+        <div class="buttons-header">
+            <div class="admin-button">Admin</div>
+            <div class="explore">Explore</div>
+        </div>
+        </div>
+
+    </header>
