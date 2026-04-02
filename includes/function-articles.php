db0e385b3490972cbeab71b9e1262937b93189be
Marwan bouhamid <marwanbouhamid123@gmail.com>
2026-04-02 11:44:35 +0200

    function of get articles from database and function of count articles wich in its categorie

---
+++ includes/function-articles.php
@@ -0,0 +1,36 @@
+<?php
+require_once "../config/database.php";
+
+function getArticlesHome(){
+    $db = new Database();
+    $conn =$db->getconnection();
+    
+    $query = "SELECT a.*, c.name as category_name, u.username as author_name 
+              FROM articles a 
+              LEFT JOIN categories c ON a.category_id = c.id 
+              LEFT JOIN users u ON a.author_id = u.id 
+              WHERE a.status = 'published' 
+              ORDER BY a.created_at DESC LIMIT 6";
+    
+    $stmt = $conn->query($query);
+    $articles =$stmt->fetchAll(PDO::FETCH_ASSOC);
+    return $articles;
+}
+
+function categoriesCount(){
+    $db = new Database();
+    $conn =$db->getconnection();
+    $cat_query = "SELECT c.name, COUNT(a.id) as count 
+              FROM categories c 
+              LEFT JOIN articles a ON c.id = a.category_id 
+              GROUP BY c.id";
+
+    $stmt = $conn->query($cat_query);
+    $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    return $categories;
+}
+
+
+
+?>
