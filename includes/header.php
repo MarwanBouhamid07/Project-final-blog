ebf5c2560c4647e5b8b386f64f3f85c9bd911f5f
Marwan bouhamid <marwanbouhamid123@gmail.com>
2026-04-03 21:44:21 +0200

    add login page

--- includes/header.php
+++ includes/header.php
@@ -7,12 +7,13 @@
                 <div class="project-name">Project Insight</div>
             </div>
             <nav>
-                <a href="">Home</a>
+                <a href="home.php">Home</a>
                 <a href="">Article</a>
                 <a href="">Showcase</a>
             </nav>
         </div>
         <div class="buttons-header">
+            <a href="login.php">Login in</a>
             <div class="admin-button">Admin</div>
             <div class="explore">Explore</div>
         </div>
